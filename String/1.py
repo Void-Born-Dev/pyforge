636a0661be2937c1a5705c91ce89d8db3c98e9b9
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-02 22:11:03 +0530

    variable

---
+++ String/1.py
@@ -0,0 +1,7 @@
+message = "Hello World"
+
+
+print(message)
+#o/p: Hello World
+
+#variable name should be as descriptive as possible. It should not be single letter or a single word. It should be a combination of words that describe the purpose of the variable.

accbb3cb7523a37d12c6ceaf0b879118b0169abe
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-02 22:11:56 +0530

    len() function

---
+++ String/2.py
@@ -0,0 +1,7 @@
+message = "Hello World"
+
+print(len(message))
+#o/p: 11
+
+#len() function is used to get the length of the string. It returns the number of characters in the string including spaces and special characters.
+

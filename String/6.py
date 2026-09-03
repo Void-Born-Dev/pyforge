7e5b197d2831a28261807ec92d4e6467ae628761
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-03 18:07:48 +0530

    few methods related to the string in python

---
+++ String/6.py
@@ -0,0 +1,10 @@
+message = "Hello World"
+
+print(message.lower())#o/p: hello world
+print(message.upper())#o/p: HELLO WORLD
+print(message.count("l"))#o/p: 3
+print(message.find('World'))#o/p: 6
+
+msg =  message.replace('World', 'Universe')
+print(msg)#o/p: Hello Universe
+

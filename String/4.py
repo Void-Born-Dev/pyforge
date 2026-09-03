4eb24b94957995853a8f4942af90a7fcd2ecda8a
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-03 18:04:28 +0530

    different ways of string declaration in python

---
+++ String/4.py
@@ -0,0 +1,10 @@
+msg = 'Hello'
+msg2 = "Hello"
+
+msg3 = 'bobby\'s car'
+msg4 = "bobby's car"
+msg5 = 'bobby said "I am a good boy"'
+
+msg6  = """bobby said "I am a good boy"""
+
+#all given above are valid string declaration in python. We can use single quotes or double quotes to declare a string. We can also use triple quotes to declare a string. Triple quotes are used to declare multi-line strings. 

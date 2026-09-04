7b816a161810ae9e8478c997a8798dda6ebc79fc
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-05 00:34:30 +0530

    resuability factor of the functions

---
+++ Functions/2.py
@@ -0,0 +1,17 @@
+# one benefit of the functions is that they allow us to reuse code without repeating ourselves 
+# for example we have to print out some text in several locations through out our program 
+
+def hello_funct():
+    print("Hello, Function")
+
+hello_funct()
+hello_funct()
+hello_funct()
+hello_funct()
+
+
+# creating a function allow us to put code with specific purpose into a single location 
+# thus we can change the required fucntionailty everywhere from one location in our program 
+
+# Now this is called keeping our program dry ("don't repeat yourself" )
+

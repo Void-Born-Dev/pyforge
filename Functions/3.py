066a9ba2d2fa0350eef0e4300a0d035ded18288e
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-05 00:35:38 +0530

    msg

---
+++ Functions/3.py
@@ -0,0 +1,16 @@
+# function allow us to work on some data and then pass our result to the whatever location from we called our function
+
+def hello_func():
+    return "Hello Function!"
+
+print(hello_func())# o/p : Hello Function! as the function hello_func is returning the string Hello Function!
+
+# we can treat this returned value as the data type it is like in this case it is string 
+
+# thus we can chain some functionalities together using it 
+
+print(hello_func().upper())# o/p : HELLO FUNCTION!
+
+# like this 
+
+

3de6a1b7c2f8a6f5fb2c3745837a3cf898b52d97
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-04 13:25:49 +0530

    loops

---
+++ loops_and_iterations/1.py
@@ -0,0 +1,32 @@
+# A loop is a a programming structure used to execute a block of code repeatedly
+
+#suppose if want to print the numbers from 0 to 9 iteratively 
+x = 0
+
+for  x in range(10):
+ print(x)
+
+#o/p 
+# 0 1 2 3 4 5 6 7 8 9 
+
+print()
+
+# A for loop is used to iterate over a sequence (such as a list, tuple, dictionary, string, or a range of numbers). It runs a specific number of times based on the length of that sequence. 
+
+nums = [1,2,3,4,5]
+
+for num in nums:
+ print(num)
+
+
+
+print()
+# A while loop is used to repeat code as long as a certain condition remains True. You use it when you don't necessarily know in advance how many times the loop will need to run
+
+x = 1
+
+while x < 6:
+ print(x)
+
+
+ 

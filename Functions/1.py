58d49dcffc5e2cdcb8367614192f4f862a75be31
Void-Born-Dev <pushpendrapatel1260@gmail.com>
2026-09-05 00:33:21 +0530

    functions

---
+++ Functions/1.py
@@ -0,0 +1,27 @@
+# a function is basically some instructions packed together that perform a specific task 
+
+def hello_func():
+    print('Hello, World')
+
+# def keyword is used to define a function 
+
+def hello()    :
+    pass
+#pass keyword  is used when we don't want to do anything in the funcion for now but it won't give any error for leaving it blank
+
+# def hello()
+# while this will give an error
+
+
+
+print(hello_func)#o/p :<function hello_func at 0x0000021575A1A340> it means a function hello_func at a certain location <----->
+
+
+
+
+# to use a function we need to call that function first by writing it's name and wrtting a parentheses bracket with it as given below 
+
+hello_func()# o/p : Hello, World
+
+
+print(hello())#o/p : None bcz this function return nothing 
